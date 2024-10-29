e0915d71ead2456aec93b6e92dcd38fa28f4a46a
Htska <112268934+Htska@users.noreply.github.com>
2024-10-28 18:45:48 -0600

    Primeros comments

--- Practica08_equipo/SRC/juegosolimpicos/src/main/java/com/equipo/juegosolimpicos/model/Atleta.java
+++ Practica08_equipo/SRC/juegosolimpicos/src/main/java/com/equipo/juegosolimpicos/model/Atleta.java
@@ -1,20 +1,68 @@
 package com.equipo.juegosolimpicos.model;
 
 import java.time.LocalDate;
-
+/**
+ * <p> Clase para el modelo de atleta </p>
+ * @author equipo.
+ */
 public class Atleta {
+    /**
+     * El número de pasaporte del atleta 
+     */
     private String numeroPasaporte;
+
+    /**
+     * Nombre del país con el que compite el atleta.
+     */
     private String nombrePais;
+
+    /**
+     * La fecha de nacimiento del atleta
+     */
     private LocalDate fechaNacimiento;
+
+    /**
+     * La nacionalidad del atleta
+     */
     private String nacionalidad;
+
+    /**
+     * Nombre del atleta
+     */
     private String nombre;
+
+    /**
+     * El primer apellido del atleta
+     */
     private String primerApellido;
+
+    /**
+     * El segundo apellido del atleta
+     */
     private String segundoApellido;
+
+    /**
+     * El género del atleta
+     */
     private char genero;
 
+    /**
+     * Constructor por omisión del atleta
+     */
     public Atleta() {
     }
 
+    /**
+     * Constructor del atleta
+     * @param numeroPasaporte el número de pasaporte
+     * @param nombrePais el nombre del país
+     * @param fechaNacimiento la fecha de nacimiento
+     * @param nacionalidad la nacionalidad
+     * @param nombre el nombre
+     * @param primerApellido el primer apellido
+     * @param segundoApellido el segundo apellido del atleta
+     * @param genero el género del atleta
+     */
     public Atleta(String numeroPasaporte,
             String nombrePais,
             LocalDate fechaNacimiento,
@@ -33,73 +81,140 @@ public class Atleta {
         this.genero = genero;
     }
 
+    /**
+     * Regresa el número de pasaporte
+     * @return String el número de pasaporte 
+     */
     public String getNumeroPasaporte() {
         return numeroPasaporte;
     }
 
+    /**
+     * Define el número de pasaporte a un atleta
+     * @param numeroPasaporte el nuevo número de pasaporte
+     */
     public void setNumeroPasaporte(String numeroPasaporte) {
         this.numeroPasaporte = numeroPasaporte;
     }
 
+    /**
+     * Regresa el nombre del país por el que participa el atleta
+     * @return String el nombre del país
+     */
     public String getNombrePais() {
         return nombrePais;
     }
 
+    /**
+     * DEfine el nombre de un país al atleta
+     * @param nombrePais el nuevo nombre del país
+     */
     public void setNombrePais(String nombrePais) {
         this.nombrePais = nombrePais;
     }
 
+    /**
+     * Regresa la fecha de nacimiento del atleta
+     * @return LocalDate la fecha de nacimiento
+     */
     public LocalDate getFechaNacimiento() {
         return fechaNacimiento;
     }
 
+    /**
+     * DEfine una nueva fecha de nacimiento al atleta
+     * @param fechaNacimiento la nueva fecha de nacimiento
+     */
     public void setFechaNacimiento(LocalDate fechaNacimiento) {
         this.fechaNacimiento = fechaNacimiento;
     }
 
+    /**
+     * Regresa la nacionalidad del atleta
+     * @return String la nacionalidad del atleta
+     */
     public String getNacionalidad() {
         return nacionalidad;
     }
 
+    /**
+     * Define una nueva nacionalidad al atleta
+     * @param nacionalidad la nueva nacionalidad
+     */
     public void setNacionalidad(String nacionalidad) {
         this.nacionalidad = nacionalidad;
     }
 
+    /**
+     * Regresa el nombre del atleta
+     * @return String el nombre del atleta
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Define un nombre al atleta
+     * @param nombre el nuevo nombre del atleta
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Rehresa el primer apellido del atleta
+     * @return String el primer apellido del atleta
+     */
     public String getPrimerApellido() {
         return primerApellido;
     }
 
+    /**
+     * define el primer apellido al atleta
+     * @param primerApellido el nuevo primer apellido
+     */
     public void setPrimerApellido(String primerApellido) {
         this.primerApellido = primerApellido;
     }
 
+    /**
+     * Regresa el segundo apellido del atleta
+     * @return String el segundo apellido
+     */
     public String getSegundoApellido() {
         return segundoApellido;
     }
 
+    /**
+     * Define el segundo apellido a un atleta
+     * @param segundoApellido el segundo apellido del atleta
+     */
     public void setSegundoApellido(String segundoApellido) {
         this.segundoApellido = segundoApellido;
     }
 
+    /**
+     * Regresa el génerp del atleta
+     * @return char el género del atleta
+     */
     public char getGenero() {
         return genero;
     }
 
+    /**
+     * Define el género del atleta
+     * @param genero el nuevo género del atleta
+     */
     public void setGenero(char genero) {
         this.genero = genero;
     }
 
-    @Override
-    public String toString() {
-        return "Atleta{" +
+    /**
+     * Regresa la representación en cadena del atleta
+     * @return String la representación en cadena
+     */
+    @Override public String toString() {
+                return "Atleta{" +
                 "numeroPasaporte='" + numeroPasaporte + '\'' +
                 ", nombrePais='" + nombrePais + '\'' +
                 ", fechaNacimiento='" + fechaNacimiento + '\'' +
--- Practica08_equipo/SRC/juegosolimpicos/src/main/java/com/equipo/juegosolimpicos/model/Disciplina.java
+++ Practica08_equipo/SRC/juegosolimpicos/src/main/java/com/equipo/juegosolimpicos/model/Disciplina.java
@@ -1,33 +1,72 @@
 package com.equipo.juegosolimpicos.model;
 
+/**
+ * <p> Clase para el modelo de Disciplina </p>
+ * @author equipo.
+ */
 public class Disciplina {
+    /**
+     * El nombre de la discipplina
+     */
     private String nombreDisciplina;
+
+    /**
+     * La categoría de la disciplina
+     */
     private String categoria;
 
+    /**
+     * Constructor por omisión
+     */
     public Disciplina() {
     }
 
+    /**
+     * Constructor de disciplina
+     * @param nombreDisciplina El nombre de la disciplina
+     * @param categoria La categoría de la disciplina
+     */
     public Disciplina(String nombreDisciplina, String categoria) {
         this.nombreDisciplina = nombreDisciplina;
         this.categoria = categoria;
     }
 
+    /**
+     * Regresa el nombre de la disciplina
+     * @return String el nombre de la disciplina
+     */
     public String getNombreDisciplina() {
         return nombreDisciplina;
     }
 
+    /**
+     * Define un nuevo nombre a la disciplina
+     * @param nombreDisciplina el nuevo nombre de la disciplina
+     */
     public void setNombreDisciplina(String nombreDisciplina) {
         this.nombreDisciplina = nombreDisciplina;
     }
 
+    /**
+     * Regresa la categoría de la disciplina
+     * @return String la categoría
+     */
     public String getCategoria() {
         return categoria;
     }
 
+    /**
+     * Define una nueva categoría a la disciplina
+     * @param categoria Regresa la categoría de la disciplina
+     */
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
 
+    /**
+     * Representación de cadena de una disciplina
+     * @return String la representación en cadena de la disciplina
+     */
     @Override
     public String toString() {
         return "Disciplina{" +
