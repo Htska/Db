72fb6f1111de6ab8303aa5f9e32c30b5c2a16738
Miguel Akira López Asano <118841701+Vnnxy@users.noreply.github.com>
2024-11-02 14:54:53 -0600

    Fixed queries and order

--- Practica09_equipo/SQL/Query.sql
+++ Practica09_equipo/SQL/Query.sql
@@ -14,7 +14,6 @@ on (a.primerapellido = e.primerapellido and a.nombredisciplina = e.nombrediscipl
 order by e.primerapellido);
 
 
-
 --2
 select nombredisciplina,idevento, idlocalidad,fecha,horainicio,duracionmaxima,costobase 
 from evento natural join entrada
@@ -30,27 +29,44 @@ from  (select numeropasaporte,count(nombredisciplina) as disciplinas
 	group by numeropasaporte) natural join atleta
 where disciplinas>1;
 
-select distinct *
-from (juez natural join supervisar natural join disciplina natural join evento) as a join (entrenador natural join evento) as e 
-on a.nacionalidad = e .nacionalidad and a.idevento != e.idevento;
+--4 
+
+-- Tabla temporal para jueces con información de la disciplina y evento.
+create temp table juezDisciplina as 
+select j.numeropasaporte as pasaporteJuez, j.nacionalidad, e.idevento, s.nombredisciplina as disciplinaJuez 
+from juez j
+join supervisar s on j.numeropasaporte = s.numeropasaporte
+join evento e on e.nombredisciplina = s.nombredisciplina
+
+--Tabla temporal para entrenadores con información de evento.
+create temp table entrenadorDisciplina as 
+select e.numeropasaporte as entrenadorPasaporte, e.nacionalidad, ev.idevento, e.nombredisciplina as disciplinaEntrenador
+from entrenador e 
+join evento ev on ev.nombredisciplina = e.nombredisciplina
+
+select distinct jd.pasaporteJuez, jd.idevento, ed.entrenadorPasaporte, ed.idevento
+from juezDisciplina jd
+join entrenadorDisciplina ed ON jd.nacionalidad = ed.nacionalidad
+where jd.idevento <> ed.idevento;
+
 
 --5
 select nombrepatrocinador
 from patrocinar
 group by nombrepatrocinador
-having count(nombredisciplina) = 1;
+having count(distinct nombredisciplina) = 1;
 
 
 --6
 select nombrepais, count(numeromedalla) as medallasganadas
 from (medalla natural join atleta)
-where medalla.tipo = 'Plata' and nombrepais='Japan'
+where medalla.tipo = 'Oro' and nombrepais='Mexico'
 group by nombrepais;
 
 --7
 select nombrepais, count(numeromedalla) as medallasganadas
 from (medalla natural join atleta)
-where medalla.tipo = 'Oro' and nombrepais='Mexico'
+where medalla.tipo = 'Plata' and nombrepais='Japan'
 group by nombrepais;
 
 --8
