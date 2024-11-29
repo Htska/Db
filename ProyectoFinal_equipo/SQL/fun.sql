c80b54622acb02d79b40623f6e4a1fc9ea15fcb6
Miguel Akira López Asano <118841701+Vnnxy@users.noreply.github.com>
2024-11-29 15:27:35 -0600

    Added 6 triggers for the agenda

--- ProyectoFinal_equipo/SQL/fun.sql
+++ ProyectoFinal_equipo/SQL/fun.sql
@@ -135,3 +135,208 @@ create trigger actualizarPrecioFinal
 before insert or update on entrada
 for each row
 execute function precioFinal();
+
+--Creamos la tabla de agendas, la cuál contiene la información de los eventos y su respectiva información
+create table agenda(
+	idEvento int,
+	idLocalidad int,
+	nombreDisciplina varchar(50),
+	fecha date,
+	horaInicio timetz,
+	duracionMaxima int,
+	precio int,
+	participantes text
+)
+--Llave primaria
+alter table agenda add constraint agenda_pkey
+primary key (idEvento); 
+-- Llaves foráneas
+alter table agenda add constraint agenda_fkey1
+foreign key (idLocalidad) references localidad(idLocalidad) on delete cascade on update cascade;
+
+alter table agenda add constraint agenda_fkey2
+foreign key (nombreDisciplina) references disciplina(nombreDisciplina) on delete cascade on update cascade;
+
+--DISPARADOR 3 creacion/ actualizacion de evento.
+--Definimos la funcion que ejecutará el trigger, esta se encarga de juntar toda la información.
+create or replace function creacionEventosAgenda()
+returns trigger as $$
+	declare
+		participantes text;
+		precio int;
+	begin
+		-- Si no hay entradas, este será nulo
+		select e.costoFinal  
+		into precio
+		from entrada e
+		where e.idEvento = new.idEvento;
+		
+		-- Si no hay participantes, la entrada será nula
+		select string_agg(p.numeroPasaporte, ', ')
+	    into participantes
+	    from participar p
+		where p.idEvento = new.idEvento;
+		-- Insertamos dentro de agenda
+		insert into agenda(idEvento, idLocalidad, nombreDisciplina, fecha, horaInicio, duracionMaxima,precio, participantes) values (new.idEvento, new.idLocalidad, new.nombreDisciplina, new.fecha, new.horaInicio, new.duracionMaxima, precio, participantes)
+		on conflict (idEvento) -- Si se llegan a tener valores distintos, este caso ocurre en los update y evita valores duplicados.
+    		do update set 
+        idLocalidad = excluded.idLocalidad,
+        nombreDisciplina = excluded.nombreDisciplina,
+        fecha = excluded.fecha,
+        horaInicio = excluded.horaInicio,
+        duracionMaxima = excluded.duracionMaxima,
+        precio = excluded.precio,
+        participantes = excluded.participantes;
+	return new;
+	end;
+$$ language plpgsql;
+-- Creamos el trigger y lo asignamos a evento
+create trigger crearEventoAgenda
+after insert or update on evento
+for each row
+execute function creacionEventosAgenda();
+
+
+--DISPARADOR 4 creacion/actualizacion de entradas.
+--Función que ejecuta el trigger, este actualiza el valor de costo final cuando una entrada sea insertada o eliminada. Se quedará el costo más pequeño. Pues consideraremos el precio como precio desde:...
+create or replace function creacionEntradasAgenda()
+returns trigger as $$
+	declare
+		precioNuevo int;
+	begin
+		--Seleccionamos el minimo, si no hay más entradas, se asignará el valor null
+		select min(e.costoFinal)  
+		into precioNuevo
+		from entrada e
+		where e.idEvento = new.idEvento;
+
+		update agenda 
+		set precio = precioNuevo
+		where idEvento = new.idEvento;
+	return new;
+	end;
+$$ language plpgsql;
+-- Creamos el trigger y lo asignamos a entrada
+create trigger crearEntradaAgenda
+after insert or update on entrada
+for each row
+execute function creacionEntradasAgenda();
+
+--DISPARADOR 5 creacion/ actualizacion de participantes.
+-- Función que ejecuta el trigger al crear o actualizar la tabla de participar.
+create or replace function creacionParticiparAgenda()
+returns trigger as $$
+	declare
+		participantesNuevos text;
+	begin	
+		-- Agregamos los nuevos participantes a nuestra cadena.
+		select string_agg(p.numeroPasaporte, ', ')
+	    into participantesNuevos
+	    from participar p
+		where p.idEvento = new.idEvento;
+
+		update agenda
+		set participantes = participantesNuevos
+		where idEvento = new.idEvento;
+	return new;
+	end;
+$$ language plpgsql;
+-- creamos el trigger y lo asignamos a participar.
+create trigger crearParticipanteAgenda
+after insert or update on participar
+for each row
+execute function creacionParticiparAgenda();
+
+--DISPARADOR 6 Eliminar evento
+-- Función que ejecuta el disparador al eliminar un evento, esta función elimina la entrada de la agenda.
+create or replace function eliminarEventoAgenda()
+returns trigger as $$
+	begin	
+		delete from agenda
+		where idEvento = old.idEvento;
+	return old;
+	end;
+$$ language plpgsql;
+-- Creamos y asignamos el disparador al eliminar en evento.
+create trigger eliminarEventoAgenda
+after delete on evento
+for each row
+execute function eliminarEventoAgenda();
+
+-- DISPARADOR 7 eliminar Entrada
+-- Función que ejecuta el disparador al eliminar una entrada. La función se encarga de actualizar el costo al más bajo disponible dentro de las entradas actuales.
+create or replace function eliminarEntradaAgenda()
+returns trigger as $$
+	declare
+		precioNuevo int;
+	begin	
+		select min(e.costoFinal)  
+		into precioNuevo
+		from entrada e
+		where e.idEvento = old.idEvento;
+
+		update agenda 
+		set precio = precioNuevo
+		where idEvento = old.idEvento;
+	return old;
+	end;
+$$ language plpgsql;
+-- Creamos y asignamos el disparador al eliminar en entrada.
+create trigger eliminarEntradaAgenda
+after delete on entrada
+for each row
+execute function eliminarEntradaAgenda();
+
+-- DISPARADOR 8 Eliminar participantes.
+-- Función que ejecuta el disparador al eliminar un participante. La función actualiza la cadena con los nuumero de pasaporte de los participantes.
+create or replace function eliminarParticiparAgenda()
+returns trigger as $$
+	declare
+		participantesNuevos text;
+	begin	
+		select string_agg(p.numeroPasaporte, ', ')
+	    into participantesNuevos
+	    from participar p
+		where p.idEvento = old.idEvento;
+
+		update agenda
+		set participantes = participantesNuevos
+		where idEvento = old.idEvento;
+	return old;
+	end;
+$$ language plpgsql;
+-- Creamos y asignamos el disparador al eliminar en participar.
+create trigger eliminarParticipanteAgenda
+after delete on participar
+for each row
+execute function eliminarParticiparAgenda();
+
+
+---- PRUEBAS PARA AGENDA -----
+
+--Cada que se realice un cambio, se puede revisar la agenda con: 
+	--select * from agenda
+
+--- PRUEBAS INSERTAR:
+--Insertamos un evento nuevo:
+	--insert into evento (idEvento, idLocalidad, nombreDisciplina, fecha, horaInicio, duracionMaxima) values (888, 10, 'Lacrosse', '7/27/2028', '21:45', 2);
+-- Para probar entrada, requerimos de una fase:
+	--insert into fase (nombreFase, idEvento) values ('Fase 2', 888);
+--Agregamos entradas, se quedará aquella con el precio más bajo:
+	--insert into entrada (folio, nombreFase, idEvento, numeroAsiento, costoBase) values ('GP10-4dzob56x04-4444', 'Fase 2', 888, 430, 1000);
+	--insert into entrada (folio, nombreFase, idEvento, numeroAsiento, costoBase) values ('GP10-4dzob56x04-4445', 'Fase 2', 888, 431, 10000);
+	--insert into entrada (folio, nombreFase, idEvento, numeroAsiento, costoBase) values ('GP10-4dzob56x04-4446', 'Fase 2', 888, 432, 100);
+--Agregamos participaciones:
+	--insert into participar (numeroPasaporte, idEvento) values ('0ILQFH4UXK', 888);
+	--insert into participar (numeroPasaporte, idEvento) values ('0AG89XNV0Q', 888);
+	--insert into participar (numeroPasaporte, idEvento) values ('1SXZRS2NX9', 888);
+
+--- PRUEBAS ACTUALIZAR:
+	--update participar set numeropasaporte = '52SSHDLP9X' where numeropasaporte = '0ILQFH4UXK'
+	--update entrada set costobase = 10 where folio = 'GP10-4dzob56x04-4444'
+	--update evento set nombredisciplina = 'Swimming' where idevento = 888
+
+--- PRUEBAS ELIMINAR: (Al eliminar la entrada de menor precio, se debe de actualizar el costo)
+	--delete from participar where numeroPasaporte = '1SXZRS2NX9'
+	--delete from entrada where folio = 'GP10-4dzob56x04-4444'
+	--delete from evento where idEvento = 888
